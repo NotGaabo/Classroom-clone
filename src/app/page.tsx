5ed10d2596c71a0379a541c4e1daf75e939d2549
notgaabo <yoensiar@gmail.com>
2026-02-09 23:14:48 -0400

    Web configs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 'use client';
 
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import Image from 'next/image';
 
 export default function HomePage() {
 
