cc4087665d4fff2dd9d21c9fcae8c3397f3113ea
delgae <maurodelanny9@gmail.com>
2026-03-09 11:33:40 -0400

    dashboard styles improved

--- src/hooks/useDashboard.ts
+++ src/hooks/useDashboard.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect, useRef } from 'react'
-import { useRouter } from 'next/router' 
+import { useRouter } from 'next/navigation'
 import { ClassDetail } from '@/types/class'
 
 interface Class {
